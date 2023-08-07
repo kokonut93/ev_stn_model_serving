17d7f1874417bd01337716e05fcd64e43e2799b3
seokyang <uoon97@gmail.com>
2023-08-07 17:01:06 +0900

    lambda_final_maybe

---
+++ Lambda-API/push_image.sh
@@ -0,0 +1,13 @@
+# aws configure
+
+# AWS Access Key ID [None]: 
+# AWS Secreat Access Key [None]: 
+# Default region name [None]: ap-northeast-2
+# Default output format [None]: json
+
+export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
+echo "export ACCOUNT_ID=${ACCOUNT_ID}" | tee -a ~/.bash_profile
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.ap-northeast-2.amazonaws.com
+
+docker tag model_api:0.0 $ACCOUNT_ID.dkr.ecr.ap-northeast-2.amazonaws.com/ecr_seokyang
+docker push $ACCOUNT_ID.dkr.ecr.ap-northeast-2.amazonaws.com/ecr_seokyang
