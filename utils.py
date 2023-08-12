6098962f5d0f4c665665793fe93d82c548894c9e
uoon97 <uoon97@hanmail.net>
2023-08-12 18:32:54 +0900

    [Add] install pymysql in Dockerfile & get floor Datetime function in utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,13 @@
-# transform json to model's input
+from datetime import datetime
 import json
 import torch
+
+
+def get_dt():
+    now = datetime.now()
+    dt = datetime(now.year, now.month, now.day, now.hour, now.minute//20*20, 00)
+    dt = dt.strftime('%Y-%m-%d %H:%M:%S')
+    return dt
 
 def json_to_tensor(json_data):
     X = []
