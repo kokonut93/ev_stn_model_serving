532c5eec9561e332eb3ed150d994adf104924771
seokyang <uoon97@gmail.com>
2023-08-17 09:52:30 +0900

    [Edit] Refactorying create_query in setting4db.py

--- setting4db.py
+++ setting4db.py
@@ -5,38 +5,12 @@ from utils import db_connect
 # Create Station's Table
 def create_station(attrs, embed):
     connection = db_connect()
-    create_query = '''
-CREATE TABLE IF NOT EXISTS station (
-Sid INT NOT NULL PRIMARY KEY, 
-Sname VARCHAR(100) NOT NULL, 
-Latitude DOUBLE NOT NULL, 
-Longitude DOUBLE NOT NULL, 
-Capacity FLOAT NOT NULL, 
-Slow_Chargers INT NOT NULL,
-Fast_Chargers INT NOT NULL,
-Mean_trip DOUBLE NOT NULL,
-Length_city DOUBLE NOT NULL,
-Length_highway DOUBLE NOT NULL,
-Length_local DOUBLE NOT NULL,
-Length_national DOUBLE NOT NULL,
-Indust_ratio DOUBLE NOT NULL,
-Etc_ratio DOUBLE NOT NULL,
-Green_ratio DOUBLE NOT NULL,
-Commerce_ratio DOUBLE NOT NULL,
-Reside_ratio DOUBLE NOT NULL,
-UMAP_1 DOUBLE NOT NULL,
-UMAP_2 DOUBLE NOT NULL,
-UMAP_3 DOUBLE NOT NULL,
-UMAP_4 DOUBLE NOT NULL,
-UMAP_5 DOUBLE NOT NULL,
-UMAP_6 DOUBLE NOT NULL,
-UMAP_7 DOUBLE NOT NULL,
-UMAP_8 DOUBLE NOT NULL
-)
-'''
+    create_query = "CREATE TABLE IF NOT EXISTS station ({}PRIMARY KEY (Sid))"
+    cols = attrs.columns.tolist()[2:] + embed.columns.tolist()[2:]
+    values = ''.join(['Sid INT NOT NULL, ' 'Sname VARCHAR(100) NOT NULL, '] + [f'{col} DOUBLE NOT NULL, ' for col in cols])
 
     with connection.cursor() as cursor:
-        cursor.execute(create_query.replace('\n', ''))
+        cursor.execute(create_query.format(values))
 
     for idx in range(attrs.shape[0]):
         attr = attrs.iloc[idx, :].values.tolist()
@@ -56,11 +30,11 @@ UMAP_8 DOUBLE NOT NULL
 def create_sequence(seq):
     connection = db_connect()
     
-    create_query = "CREATE TABLE IF NOT EXISTS sequence ({} PRIMARY KEY (Time))"
-    cols = ''.join(['Time DATETIME NOT NULL, '] + [f'Sid_{col} INT NOT NULL, ' for col in seq.columns[1:]])
+    create_query = "CREATE TABLE IF NOT EXISTS sequence ({}PRIMARY KEY (Time))"
+    values = ''.join(['Time DATETIME NOT NULL, '] + [f'Sid_{col} INT NOT NULL, ' for col in seq.columns[1:]])
 
     with connection.cursor() as cursor:
-        cursor.execute(create_query.format(cols))
+        cursor.execute(create_query.format(values))
 
     for _, row in seq.iterrows():
         values = tuple(row.values.tolist())
@@ -77,24 +51,14 @@ def create_sequence(seq):
 # Create Occupancy's Table
 def create_occupancy():
     connection = db_connect()
-    create_query = '''
-CREATE TABLE occupancy (
-Sid INT NOT NULL PRIMARY KEY, 
-Occupancy_20 INT NOT NULL,
-Occupancy_40 INT NOT NULL,
-Occupancy_60 INT NOT NULL,
-Occupancy_80 INT NOT NULL,
-Occupancy_100 INT NOT NULL,
-Occupancy_120 INT NOT NULL,
-FOREIGN KEY (sid) REFERENCES station(sid)
-)
-'''
+    create_query = "CREATE TABLE IF NOT EXISTS occupancy ({}FOREIGN KEY (Sid) REFERENCES station(Sid))"
+    values = ''.join(['Sid INT NOT NULL PRIMARY KEY, '] + [f'Occupancy_{i} INT NOT NULL, ' for i in range(20, 121, 20)])
 
     select_query = "SELECT sid FROM station"
 
 
     with connection.cursor() as cursor:
-        cursor.execute(create_query.replace('\n', ''))
+        cursor.execute(create_query.format(values))
         cursor.execute(select_query)
         result = cursor.fetchall()
 
@@ -108,7 +72,7 @@ FOREIGN KEY (sid) REFERENCES station(sid)
     connection.close()
 
 
-def __main__():
+if __name__=="__main__":
     attrs = pd.read_csv('data/station_attrs.csv')
     embed = pd.read_csv('data/station_embed.csv')
     seq = pd.read_csv('data/station_seq.csv')
